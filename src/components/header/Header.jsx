49f53585a836e432a7fa2ea26c6bafae4a99c760
Karam <karamguliyev10@gmail.com>
2022-03-05 14:22:48 +0300

    Responsive Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import ME from '../../assets/loading.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Karam GULIYEV</h1>
+      <h5 className="text-light">FULLSTACK DEVELOPER</h5>
+      <CTA />
+      <HeaderSocials />
+
+      <div className="me">
+        <img src={ME} alt="" />
+      </div>
+      <a href='#contact' className='scroll__down'>
+        Scroll Down
+      </a>
+      </div>
+    </header>
   )
 }
 
