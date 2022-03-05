61a2b40cd736961cd563bc674010c26fa1348691
Karam <karamguliyev10@gmail.com>
2022-03-05 15:58:48 +0300

    Responsive About Component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,55 @@
 import './about.css';
 import React from 'react'
+import ME from '../../assets/smile.jpg'
+import {BsAward} from 'react-icons/bs'
+import {FiUsers} from 'react-icons/fi'
+import {AiOutlineFolder} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <section id='about'>
+      <h5>Get to Know</h5>  
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <img src={ME} alt="About Image" className='about__me-image'/>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+
+            <article className="about__card">
+              <BsAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Yers Working</small>
+            </article>  
+
+            <article className="about__card">
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>  
+
+            <article className="about__card">
+              <AiOutlineFolder className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>  
+          </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit quam id, 
+            quasi tempore molestiae saepe in temporibus rem, ex nihil, 
+            dolore ut ipsa ea corrupti placeat tempora ducimus amet numquam?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Lets Talk</a>
+
+        </div>
+      </div>
+    </section>
+  ) 
 }
 
 export default About
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css';
+import {AiOutlineHome} from 'react-icons/ai'
+import {BiUserCircle} from 'react-icons/bi'
+import {BiBrain} from 'react-icons/bi'
+import {MdMiscellaneousServices} from 'react-icons/md'
+import {RiCustomerServiceLine} from 'react-icons/ri'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle/></a>
+      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBrain/></a>
+      <a href='#services' onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdMiscellaneousServices/></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiCustomerServiceLine/></a>
+    </nav>
   )
 }
 
