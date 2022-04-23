7d50f19d731ab61317e74945630beb36ece5e637
Karam <karamguliyev10@gmail.com>
2022-04-23 14:02:57 +0300

    Responsive Portfolio Section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,101 @@
-import React from 'react'
-import './portfolio.css';
+import React from "react";
+import "./portfolio.css";
+import img from "../../assets/smile.jpg";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img} alt="" />
+          </div>
+          <h3>this is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#" className="btn" target="_blank">
+              GitHUB
+            </a>
+            <a href="#" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img} alt="" />
+          </div>
+          <h3>this is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#" className="btn" target="_blank">
+              GitHUB
+            </a>
+            <a href="#" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img} alt="" />
+          </div>
+          <h3>this is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#" className="btn" target="_blank">
+              GitHUB
+            </a>
+            <a href="#" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img} alt="" />
+          </div>
+          <h3>this is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#" className="btn" target="_blank">
+              GitHUB
+            </a>
+            <a href="#" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img} alt="" />
+          </div>
+          <h3>this is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#" className="btn" target="_blank">
+              GitHUB
+            </a>
+            <a href="#" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img} alt="" />
+          </div>
+          <h3>this is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="#" className="btn" target="_blank">
+              GitHUB
+            </a>
+            <a href="#" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
