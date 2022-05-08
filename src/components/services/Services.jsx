b68434d5f18ace26fa8afc5ec896e2b1873d9825
Karam <karamguliyev10@gmail.com>
2022-05-08 16:58:54 +0300

    Mapping Services section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./services.css";
 import { BiCheck } from "react-icons/bi";
+const webDeveloper = [
+  {
+    id: "webDeveloper",
+    title: "Web Developer",
+    p1: "This is a test message",
+    index: "1",
+  },
+];
+const graphicDesign = [
+  {
+    id: "graphicDesign",
+    title: "Graphic Design",
+    p1: "This is a test message",
+    index: "2",
+
+  },
+];
 
 const Services = () => {
   return (
@@ -9,156 +26,44 @@ const Services = () => {
       <h2>Services</h2>
 
       <div className="container services__container">
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX Design</h3>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-          </ul>
-        </article>
-        <article className="service">
-          <div className="service__head">
-            <h3>Web Developent</h3>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" size={70} />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-          </ul>
-        </article>
-        <article className="service">
-          <div className="service__head">
-            <h3>SMM</h3>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>
-                Karam Guliyev Karam Guliyev Karam Guliyev Karam Guliyev Karam
-                Guliyev
-              </p>
-            </li>
-          </ul>
-        </article>
+        {webDeveloper.map(({ id, p1, title }) => {
+          return (
+            <article id={id} className="service">
+              <div className="service__head">
+                <h3>{title}</h3>
+              </div>
+              <ul className="service__list">
+                <li>
+                  <BiCheck className="service__list-icon" size={50}/>
+                  <p>{p1}</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" size={50}/>
+                  <p>{p1}</p>
+                </li>
+                <li>
+                  <BiCheck className="service__list-icon" width="50px"/>
+                  <p>{p1}</p>
+                </li>
+              </ul>
+            </article>
+          );
+        })}
+        {graphicDesign.map(({ id, p1, title }) => {
+          return (
+            <article id={id} className="service">
+              <div className="service__head">
+                <h3>{title}</h3>
+              </div>
+              <ul className="service__list">
+                <li>
+                  <BiCheck className="service__list-icon" size={50}/>
+                  <p>{p1}</p>
+                </li>
+              </ul>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
